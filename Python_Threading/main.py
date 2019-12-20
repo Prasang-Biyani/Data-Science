732bf96211f907b97c55c2f0caafdb339c2b4e99
Prasang Biyani <prasang@promactinfo.com>
2019-12-20 15:38:26 +0530

    added in python threading module

--- Python_Threading/main.py
+++ Python_Threading/main.py
@@ -1,25 +1,39 @@
 import time
 import threading
+import concurrent.futures
 
 start = time.perf_counter()
 
-def do_something():
-    print('Sleeping 1 second...')
-    time.sleep(1)
-    print('Done Sleeping...')
-
+def do_something(id_func):
+    print('do_something ' ,id_func)
+    time.sleep(id_func)
+    return 'Done Sleeping for id ' + str(id_func)
 
 # do_something()
 # do_something()
 
-t1 = threading.Thread(target=do_something)
-t2 = threading.Thread(target=do_something)
+# threads = []
+# for i in range(10):
+#     t = threading.Thread(target=do_something, args=[i])
+#     t.start()
+#     threads.append(t)
 
-t1.start()
-t2.start()
+# for thread in threads:
+#     thread.join()
 
-t1.join()
-t2.join()
+# with concurrent.futures
+# with concurrent.futures.ThreadPoolExecutor() as executor:
+#     # f1 = executor.submit(do_something, 1)
+#     # print(f1.result())
+#     results = [executor.submit(do_something, i) for i in range(10)]
+#     for f in concurrent.futures.as_completed(results):
+#         print(f.result())
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs = [5, 4, 3, 2, 1]
+    results = executor.map(do_something, secs)
+    for result in results:
+        print(result)
 
 finish = time.perf_counter()
 print(f'Finished in {round(finish - start, 2)} second(s)')
