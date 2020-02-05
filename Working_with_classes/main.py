b95c402a8ac8e97e7a1bee092883bd77a88001d7
Prasang Biyani <prasang@promactinfo.com>
2020-02-05 15:39:36 +0530

    added Inheritance concept

--- Working_with_classes/main.py
+++ Working_with_classes/main.py
@@ -15,15 +15,62 @@ class Employee:
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
+    
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+    
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+    
+    @staticmethod
+    def  is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
-emp1 = Employee('Prasang', 'Biyani', 50000)
-emp2 = Employee('Test', 'Testing1', 60000)
 
-print(Employee.__dict__)
-print(emp1.raise_amount)
-Employee.raise_amount = 1.08
-print(emp1.raise_amount)
-print(emp2.raise_amount)
+class Developer(Employee):
 
-print(Employee.num_of_emps)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.full_name())
+        
+dev_1 = Developer('Prasang', 'Biyani', 50000, 'Python')
+dev_2 = Developer('Test', 'Testing1', 60000, 'Java')
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+mgr_1.print_emps()
+
+mgr_1.add_emp(dev_2)
+
+print(isinstance(mgr_1, Employee)) # True
+print(issubclass(Manager, Employee)) # True
+print(issubclass(Developer, Manager)) # False
+
+
+
+
 
